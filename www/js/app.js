4cc259aa3bbf57265ec2451261ce67a0bf24cdfd
Kirill <kam4513@gmail.com>
2016-10-13 02:37:46 +0300

    add filter

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services'])
+var app = angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services','ngSanitize'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
 
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
@@ -14,3 +14,54 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services']
     }
   });
 })
+
+/*app.directive("contenteditable", function() {
+  return {
+    restrict: "A",
+    require: "ngModel",
+    link: function(scope, element, attrs, ngModel) {
+
+      function read() {
+        var html = element.html();
+        html = html.replace(/&nbsp;/g, "\u00a0");
+        ngModel.$setViewValue(html);
+      }
+
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || "");
+      };
+
+      element.bind("blur keyup change", function() {
+        scope.$apply(read);
+      });
+    }
+  };
+});*/
+
+
+app.filter('tweetLinky',['$filter', '$sce',
+  function($filter, $sce) {
+    return function(text, target) {
+      if (!text) return text;
+
+      var replacedText = $filter('linky')(text, target);
+      var targetAttr = "";
+      if (angular.isDefined(target)) {
+        targetAttr = ' target="' + target + '"';
+      }
+
+      // replace #hashtags
+      var replacePattern1 = /(^|\s)#(\w*[a-zA-Z_]+\w*)/gim;
+      replacedText = replacedText.replace(replacePattern1, '$1<a href="https://twitter.com/search?q=%23$2"' + targetAttr + '>#$2</a>');
+
+      // replace @mentions
+      var replacePattern2 = /(^|\s)\@(\w*[a-zA-Z_]+\w*)/gim;
+      replacedText = replacedText.replace(replacePattern2, '$1<a href="https://twitter.com/$2"' + targetAttr + '>@$2</a>');
+
+      $sce.trustAsHtml(replacedText);
+      return replacedText;
+    };
+  }
+]);
+
+
